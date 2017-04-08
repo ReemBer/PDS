acc562d92154873604beafb8ab3bc6dc81916449
Tarasevich Vladislav <tarasevich.vlad.97@gmail.com>
2017-04-08 18:50:22 +0300

    Enumeration Cargo completed

---
+++ src/Ship/Cargo.java
@@ -0,0 +1,7 @@
+package Ship;
+
+/**
+ * Created by Tarasevich vladislav on 08.04.2017.
+ * This enumeration used to designate freight on ships and port warehouses
+ */
+public enum Cargo {GAS, OIL, FOOD, CARS};
