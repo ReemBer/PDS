9177fecfb2075c329b3c42d255db56f9adfa00f7
Tarasevich Vladislav <tarasevich.vlad.97@gmail.com>
2017-04-09 22:56:08 +0300

    Initoalisation of Warehouse class

---
+++ src/Port/Warehouse.java
@@ -0,0 +1,12 @@
+package Port;
+
+/**
+ * Created by Tarasevich Vladislav on 09.04.2017.
+ * @author Tarasevich Vladislav
+ * This class used by Port class to store
+ * information about existing goods.
+ */
+public class Warehouse
+{
+    
+}
