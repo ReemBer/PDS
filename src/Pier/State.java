214270e1741aa609fb265a421aba3910641d8bd3
Tarasevich Vladislav <tarasevich.vlad.97@gmail.com>
2017-04-09 19:35:49 +0300

    Enumeration Stati completed

---
+++ src/Pier/State.java
@@ -0,0 +1,9 @@
+package Pier;
+
+/**
+ * Created by Tarasevich Vladislav on 09.04.2017.
+ * @author Tarasevich Vladislav
+ * This Enumeration used by the Pier class to saving
+ * current satate of current Pier
+ */
+public enum State {LOADING, UNLOADING, WAITING}
