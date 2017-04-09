d1254cf59472f6d2653af7882a29e5b67aa6b780
Tarasevich Vladislav <tarasevich.vlad.97@gmail.com>
2017-04-10 00:54:13 +0300

    The begining of designing of the process method

--- src/Pier/Pier.java
+++ src/Pier/Pier.java
@@ -15,6 +15,8 @@ public class Pier extends Thread
     private State state;
     private Ship  ship;
 
+    private TimeCost timeCost;
+
     public Pier()
     {
         parentPort = null;
@@ -53,5 +55,68 @@ public class Pier extends Thread
     public void setShip(Ship ship)
     {
         this.ship = ship;
+    }
+
+    /**
+     * This method are designed to processing currentShip
+     * and update progressBar state
+     */
+    public void process() // TODO: 10.04.2017 Доделать  
+    {
+        int fullTime = calculateTime();
+        int steep;
+
+        switch(ship.getCargo())
+        {
+            case OIL:
+            {
+                steep = timeCost.FOR_OIL;
+            }
+            case GAS:
+            {
+                steep = timeCost.FOR_GAS;
+            }
+            case FOOD:
+            {
+                steep = timeCost.FOR_FOOD;
+            }
+            case CARS:
+            {
+                steep =  timeCost.FOR_CARS;
+            }
+        }
+    }
+
+
+    /**
+     * This method used by process method to get full processing time
+     * for current ship
+     * @return full time, needed to process current Ship
+     */
+    private int calculateTime()
+    {
+        switch(ship.getCargo())
+        {
+            case OIL:
+            {
+                return timeCost.FOR_OIL * ship.getCount();
+            }
+            case GAS:
+            {
+                return timeCost.FOR_GAS * ship.getCount();
+            }
+            case FOOD:
+            {
+                return timeCost.FOR_FOOD * ship.getCount();
+            }
+            case CARS:
+            {
+                return timeCost.FOR_CARS * ship.getCount();
+            }
+            default:
+            {
+                return 0;
+            }
+        }
     }
 }
