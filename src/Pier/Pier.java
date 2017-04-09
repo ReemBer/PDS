40a0a5357253ca1d840ab41e893c01da69f09459
Tarasevich Vladislav <tarasevich.vlad.97@gmail.com>
2017-04-09 19:31:44 +0300

    Initialisation of class Pier

---
+++ src/Pier/Pier.java
@@ -0,0 +1,12 @@
+package Pier;
+
+/**
+ * Created by Tarasevich Vladislav on 09.04.2017.
+ * @author Tarasevich Vladislav
+ * This class used by the Port clas to communicate between
+ * Ship and Port
+ */
+public class Pier
+{
+    
+}
