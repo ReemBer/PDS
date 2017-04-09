3ae7b73a72340a3760ef93b2dde70cf5f0ce9427
Tarasevich Vladislav <tarasevich.vlad.97@gmail.com>
2017-04-09 23:38:53 +0300

    Added base functional without FileWorker system

--- src/Port/Warehouse.java
+++ src/Port/Warehouse.java
@@ -1,5 +1,7 @@
 package Port;
 
+import Ship.Cargo;
+
 /**
  * Created by Tarasevich Vladislav on 09.04.2017.
  * @author Tarasevich Vladislav
@@ -8,5 +10,114 @@ package Port;
  */
 public class Warehouse
 {
-    
+    private final String DEFAULT_PATH_TO_GOODS = "Goods.txt";
+
+    private String pathToGoods = DEFAULT_PATH_TO_GOODS;
+
+    private int oilCount;
+    private int gasCount;
+    private int foodCount;
+    private int carsCount;
+
+    public Warehouse()
+    {
+        readCountsOfGoods();
+    }
+
+    public Warehouse(int oilCount, int gasCount, int foodCount, int carsCount)
+    {
+        this.oilCount = oilCount;
+        this.gasCount = gasCount;
+        this.foodCount = foodCount;
+        this.carsCount = carsCount;
+    }
+
+    public synchronized boolean takeCargo(Cargo cargo, int count)
+    {
+        switch(cargo)
+        {
+            case OIL:
+            {
+                if(count > oilCount) return false;
+
+                oilCount -= count;
+                break;
+            }
+            case GAS:
+            {
+                if(count > gasCount) return false;
+
+                gasCount -= count;
+                break;
+            }
+            case FOOD:
+            {
+                if(count > foodCount) return false;
+
+                foodCount -= count;
+                break;
+            }
+            case CARS:
+            {
+                if(count > carsCount) return false;
+
+                carsCount -= count;
+                break;
+            }
+        }
+
+        return true;
+    }
+
+    public synchronized void putCargo(Cargo cargo, int count)
+    {
+        switch(cargo)
+        {
+            case OIL:
+            {
+                oilCount += count;
+                break;
+            }
+            case GAS:
+            {
+                gasCount += count;
+                break;
+            }
+            case FOOD:
+            {
+                foodCount += count;
+                break;
+            }
+            case CARS:
+            {
+                carsCount += count;
+                break;
+            }
+        }
+    }
+
+    public synchronized int getOilCount()
+    {
+        return oilCount;
+    }
+
+    public synchronized int getGasCount()
+    {
+        return  gasCount;
+    }
+
+    public synchronized int getFoodCount()
+    {
+        return foodCount;
+    }
+
+    public synchronized int getCarsCount()
+    {
+        return carsCount;
+    }
+
+    private void readCountsOfGoods()
+    {
+
+    }
 }
