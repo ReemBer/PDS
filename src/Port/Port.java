7869678bf2d8bf715d3f2af2567a6f0273aae243
Tarasevich Vladislav <tarasevich.vlad.97@gmail.com>
2017-04-09 17:16:25 +0300

    Initialisation of class Port

---
+++ src/Port/Port.java
@@ -0,0 +1,8 @@
+package Port;
+
+/**
+ * Created by Tarasevich Vladislav on 09.04.2017.
+ * This class used to store the state of the warehouse and work with ships through the piers
+ */
+public class Port {
+}
