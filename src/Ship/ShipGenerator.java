b864569213f15a31dba8f79e02aa402ac90e87bd
Tarasevich Vladislav <tarasevich.vlad.97@gmail.com>
2017-04-10 21:03:15 +0300

    Initialisation of the ShipGenerator class

---
+++ src/Ship/ShipGenerator.java
@@ -0,0 +1,15 @@
+package Ship;
+
+/**
+ * Created by Tarasevich Vladislav on 10.04.2017.
+ * @author Tarasevich Vladislav
+ * @author +375336492547
+ * @version 1.0
+ *
+ * This class needed to generate random information
+ * to fill fields of data of Ship.
+ */
+public class ShipGenerator extends Thread
+{
+
+}
