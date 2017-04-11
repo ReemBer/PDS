b6040ca7c89550e8191843ea08ee5f953dfedad1
Tarasevich Vladislav <tarasevich.vlad.97@gmail.com>
2017-04-11 04:35:03 +0300

    The MainWindow class has been initialized

---
+++ src/GUI/MainWindow.java
@@ -0,0 +1,19 @@
+package GUI;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Created by Tarasevich Vladislav on 11.04.2017.
+ * @author name :  Tarasevich Vladislav
+ * @author gmail : tarasevich.vlad.97@gmail.com
+ * @author phone : +375336492547
+ */
+public class MainWindow extends Application
+{
+    @Override
+    public void start(Stage primaryStage) throws Exception
+    {
+        
+    }
+}
