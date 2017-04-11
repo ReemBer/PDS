c954f9fe8db37af2d591948cc7dc17939d5c8d8c
Tarasevich Vladislav <tarasevich.vlad.97@gmail.com>
2017-04-11 04:27:07 +0300

    The Runner class has been initialized

---
+++ src/Manager/Runner.java
@@ -0,0 +1,15 @@
+package Manager;
+
+/**
+ * Created by Tarasevich Vladislav on 11.04.2017.
+ * @author name : Tarasevich Vladislav
+ * @author gmail : tarasevich.vlad.97@gmail.com
+ * @author phone : +375336492547
+ */
+public class Runner
+{
+    public static void main(String[] args)
+    {
+        
+    }
+}
