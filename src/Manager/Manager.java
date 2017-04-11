e79d431c3e292246dc32a7a27eff262e42113ef6
Tarasevich Vladislav <tarasevich.vlad.97@gmail.com>
2017-04-11 08:32:44 +0300

    The Manager class has been initialized

---
+++ src/Manager/Manager.java
@@ -0,0 +1,14 @@
+package Manager;
+
+/**
+ * Created by Tarasevich Vladislav on 11.04.2017.
+ * @author name  : Tarasevich Vladislav
+ * @author gmail : tarasevich.vlad.97@gmail.com
+ * @author phone : +375336492547
+ * This is the main classm, used to do communication
+ * between GUI and working nodes of program
+ */
+public class Manager
+{
+    
+}
