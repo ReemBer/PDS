f615bdafb01204d7d568ade290aedff293cca7fb
Tarasevich Vladislav <tarasevich.vlad.97@gmail.com>
2017-04-12 18:46:26 +0300

    The Ship class (version 2.0) has been initialized

---
+++ src/Ship/Ship.java
@@ -0,0 +1,13 @@
+package Ship;
+
+/**
+ * Created by Tarasevich Vladislav on 12.04.2017.
+ * @author name : Tarasevich Vladislav
+ * @author mail : tarasevich.vlad.97@gmail.com
+ * @version 2.0
+ * This is the Class-model for TableView.
+ */
+public class Ship
+{
+
+}
