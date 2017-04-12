aa62c5da1a2a42bc6c9535d18f1567e5b2bdb6f9
Tarasevich Vladislav <tarasevich.vlad.97@gmail.com>
2017-04-12 21:49:09 +0300

    Classes : ShipRequests -Overview and -OverviewController-- initialized

---
+++ src/Manager/View/ShipRequestsOverviewController.java
@@ -0,0 +1,11 @@
+package Manager.View;
+
+/**
+ * Created by Tarasevich Vladislav on 12.04.2017.
+ * @author name  : Tarasevich vladislav
+ * @author gmail : tarasevich.vlad.97@gmail.com
+ */
+public class ShipRequestsOverviewController
+{
+    
+}
