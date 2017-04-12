01a88fbb5951d403db395a3dc12cd83d37a41ab8
Tarasevich Vladislav <tarasevich.vlad.97@gmail.com>
2017-04-12 22:01:04 +0300

    Added fields into the ShipRequestsOverviewController class

--- src/Manager/View/ShipRequestsOverviewController.java
+++ src/Manager/View/ShipRequestsOverviewController.java
@@ -1,5 +1,10 @@
 package Manager.View;
 
+import Manager.Manager;
+import Ship.*;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
 /**
  * Created by Tarasevich Vladislav on 12.04.2017.
  * @author name  : Tarasevich vladislav
@@ -7,5 +12,22 @@ package Manager.View;
  */
 public class ShipRequestsOverviewController
 {
+    @FXML
+    private TableView<Ship> shipRequestTable;
+
+    @FXML
+    private TableColumn<Ship, String> nameColumn;
+
+    @FXML
+    private TableColumn<Ship, Cargo> cargoColumn;
+
+    @FXML
+    private TableColumn<Ship, Integer> countColumn;
+
+    @FXML
+    private TableColumn<Ship, Boolean> isLoadRequestColumn;
+
+    private Manager mainProgramObject;
+
     
 }
