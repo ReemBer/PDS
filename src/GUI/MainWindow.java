91a705308c3a40d4ca151e2e172b62f0a31fe910
Tarasevich Vladislav <tarasevich.vlad.97@gmail.com>
2017-04-13 23:34:17 +0300

    Added simple window with tableViewController

--- src/GUI/MainWindow.java
+++ src/GUI/MainWindow.java
@@ -1,7 +1,16 @@
 package GUI;
 
+import Manager.Manager;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import sun.applet.Main;
+
+import java.io.IOException;
 
 /**
  * Created by Tarasevich Vladislav on 11.04.2017.
@@ -11,9 +20,52 @@ import javafx.stage.Stage;
  */
 public class MainWindow extends Application
 {
+    private Stage primaryStage;
+    private AnchorPane rootLayout;
+    private Manager mainPrgramObject;
+
+    //public MainWindow(Manager mainProgramObject)
+    //{
+    //    this.mainProgramObject = mainProgramObject;
+    //}
+
     @Override
-    public void start(Stage primaryStage) throws Exception
+    public void start(Stage primaryStage)
     {
-        
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Port Dispatch System");
+
+        initRootLayout();
     }
+
+    public void initRootLayout()
+    {
+        try
+        {
+            Parent root = FXMLLoader.load(getClass().getResource("View/ShipRequestsOverview.fxml"));
+
+            primaryStage.setScene(new Scene(root));
+            primaryStage.show();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+//    public void showShipRequestsOverview()
+//    {
+//        try
+//        {
+//            FXMLLoader loader = new FXMLLoader();
+//            loader.setLocation(MainWindow.class.getResource("View/ShipRequestsOverview.fxml"));
+//            AnchorPane personOverview = (AnchorPane) loader.load();
+//
+//            rootLayout.add(personOverview);
+//        }
+//        catch(IOException e)
+//        {
+//            e.printStackTrace();
+//        }
+//    }
 }
--- src/GUI/View/ShipRequestsOverviewController.java
+++ src/GUI/View/ShipRequestsOverviewController.java
@@ -1,7 +1,9 @@
-package Manager.View;
+package GUI.View;
 
 import Manager.Manager;
 import Ship.*;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
@@ -37,10 +39,21 @@ public class ShipRequestsOverviewController
     @FXML
     private void initialize()
     {
+        test();
+
         nameColumn.setCellValueFactory(cellData -> cellData.getValue().nameProperty());
         cargoColumn.setCellValueFactory(cellData -> cellData.getValue().cargoProperty());
         countColumn.setCellValueFactory(cellData -> cellData.getValue().countProperty().asObject());
         isLoadRequestColumn.setCellValueFactory(cellData -> cellData.getValue().isLoadRequestProperty());
+    }
+
+    private void test()
+    {
+        ObservableList<Ship> t = FXCollections.observableArrayList();
+        t.add(new Ship());
+        t.add(new Ship("IGOR", Cargo.GAS, 100, true));
+
+        shipRequestTable.setItems(t);
     }
 
     public void setMainProgramObject(Manager mainProgramObject)
